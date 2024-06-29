f0914724a386d889853552a4353ef59c8daf51e3
mkowack <kowackag@gmail.com>
2024-06-29 15:29:56 +0200

    .gitignore fix

--- src/components/atoms/Button/Button.test.tsx
+++ src/components/atoms/Button/Button.test.tsx
@@ -6,19 +6,19 @@ describe("Button", () => {
 	it("Button displays appropriate text", () => {
 		render(<Button text="Login" type="submit" />);
 
-		const button = screen.getByRole("button");
+		const button = screen.getByTestId("btn-submit");
 		expect(button).toHaveTextContent("Login");
 	});
 
 	it("Button has appropriate type", () => {
-		render(<Button text="Login" type="submit" />);
+		render(<Button text="Login" type="button" />);
 
-		const button = screen.getByRole("button");
-		expect(button).toHaveAttribute("type", "submit");
+		const button = screen.getByTestId("btn-button");
+		expect(button).toHaveAttribute("type", "button");
 	});
 
 	it("Button renders correctly", () => {
-		const wrapper = render(<Button text="Login" type="submit" />)
-		expect(wrapper.container).toMatchSnapshot()
-	})
+		const wrapper = render(<Button text="Login" type="submit" />);
+		expect(wrapper.container).toMatchSnapshot();
+	});
 });
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -3,6 +3,7 @@ export const Button = ({ text, type }: { text: string; type: HTMLButtonElement["
 		<button
 			className="my-2 w-full rounded-lg bg-bright-rose text-center font-primaryMedium text-sm uppercase leading-10 tracking-widest text-neutral-100 shadow-btn"
 			type={type}
+			data-testid={`btn-${type}`}
 		>
 			{text}
 		</button>
